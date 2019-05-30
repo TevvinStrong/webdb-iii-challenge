14061cbb9ab40bdc6bc8e3c3a52d1bc8170da950
Tevvin <Tevvin7395@icloud.com>
2019-05-30 02:40:06 -0400

    implemented 001-cohorts see js file

--- migrations/20190530002610_cohorts.js
+++ migrations/20190530002610_cohorts.js
@@ -1,12 +1,13 @@
 // Knex migrate:make <name> to create migration.
 // knex migrate:latest to add tables to our database(sqlite3)
-// knex migrate:rollback to revert back to the beginning
+// knex migrate:rollback to revert back to the last migration performed
 
+// Create table
 exports.up = async function (knex) {
-    // Create (cohorts) Tables.
     await knex.schema.createTable('cohorts', (tbl) => {
-        tbl.increments('id'); // when calling increments to id = primary key & auto increments.
-        tbl.string('name', 128).unique().notNullable();
+        // Columns
+        tbl.increments('id'); // id column = increments/primary key.
+        tbl.string('name', 128).unique().notNullable(); // name column = unique/not null
     })
 };
 
--- migrations/20190530014727_students.js
+++ migrations/20190530014727_students.js
@@ -1,9 +1,15 @@
+// Knex migrate:make <name> to create migration.
+// knex migrate:latest to add tables to our database(sqlite3)
+// knex migrate:rollback to revert back to last migration performed
 
+// Create table
 exports.up = async (knex) => {
     await knex.schema.createTable('students', (tbl) => {
-        tbl.increments('id'); // when calling increments to id = primary key & auto increments.
-        tbl.string('name', 128).unique().notNullable();
-        tbl.integer('cohort_id');
+        // Columns
+        tbl.increments('id');  // id column = increments/primary key.
+        tbl.string('name', 128).unique().notNullable(); // name column = unique/not null
+        // Foeign key connects to the id column in the cohorts table.
+        tbl.integer('cohort_id').unsigned().refrences('id').inTable('cohorts').onDelete('CASCADE').onUpdate('CASCADE');
     })
 };
 
