ba08b396558d3b68dd95dbfc6cf21ce6446f476e
Tevvin <Tevvin7395@icloud.com>
2019-05-30 21:21:10 -0400

    Added basic endpoint structure

--- index.js
+++ index.js
@@ -18,7 +18,29 @@ server.get('/', (req, res) => {
 });
 
 // Endpoints
+server.post('/api/cohorts', (req, res) => {
 
+});
+
+server.get('/api/cohorts', (req, res) => {
+
+});
+
+server.get('/api/cohorts/:id', (req, res) => {
+
+});
+
+server.get('/api/cohorts/:id/students', (req, res) => {
+
+});
+
+server.put('/api/cohorts/:id', (req, res) => {
+
+});
+
+server.delete('/api/cohorts/:id', (req, res) => {
+
+});
 
 // Stretch
 
